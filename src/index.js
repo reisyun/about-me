40d8f038bf74003f8f07ad5cde1d322fc692138a
reisyun <reisyun7@gmail.com>
2020-06-10 21:42:59 +0900

    chore: Change folder structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 (async function createProjectSection() {
   const projectWrapperEle = document.getElementById("project");
 
-  const project = await fetch("./project.json")
+  const project = await fetch("../src/project.json")
     .then((res) => res.json())
     .then(({ project }) => project);
 
@@ -10,7 +10,7 @@
     const linksEle = links.map(
       ({ name, link }) =>
         `<a title="${name}" target="_blank" href="${link}">
-          <object type="image/svg+xml" data="./assets/icons/${name}.svg">
+          <object type="image/svg+xml" data="../assets/icons/${name}.svg">
             ${name}
           </object>
         </a>`
@@ -18,13 +18,11 @@
 
     return `<article class="card shadow_box">
         <div class="card__image">
-          <img src="./assets/images/work/${title}.png" />
+          <img src="../assets/images/work/${title}.png" />
         </div>
         <div class="card__content">
           <h2 class="card__content--title">${title}</h2>
-
           <p class="card__content--desc">${desc}</p>
-          
           <ul class="card__content--learn">${learnEle.join("")}</ul>
           </div>
           <div class="card__links">
@@ -48,7 +46,7 @@
     ({ file, name }) =>
       `<figure class="item">
         <div class="item__logo shadow_box">
-          <img src="./assets/images/skills/${file}" />
+          <img src="../assets/images/skills/${file}" />
         </div>
         <figcaption class="item__caption">${name}</figcaption>
       </figure>`
